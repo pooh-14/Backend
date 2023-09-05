318ab4e41f3fbe8b8961197b93f81a8791a5d20a
pooh-14 <poojavetal14@gmail.com>
2023-09-05 10:46:03 +0530

    done

--- frontend/src/Components/ApiConfig/index.js
+++ frontend/src/Components/ApiConfig/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("token"));
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://e-commerce-f7rw.onrender.com",
   headers: { Authorization: `Bearer ${token}` },
 });
 
